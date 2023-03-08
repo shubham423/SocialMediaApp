ddeb39f98829c35f54023f5a526632b956240988
shubham423 <shubh991172@gmail.com>
2023-03-08 10:29:01 +0530

    login screen

---
+++ app/src/main/java/com/example/socialmediaapp/presentation/components/StandardTextField.kt
@@ -0,0 +1,102 @@
+package com.example.socialmediaapp.presentation.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import com.example.socialmediaapp.R
+
+@Composable
+fun StandardTextField(
+    modifier: Modifier = Modifier,
+    text: String = "",
+    hint: String = "",
+    maxLength: Int = 40,
+    error: String = "s",
+    keyboardType: KeyboardType = KeyboardType.Text,
+    showPasswordToggle: Boolean = false,
+    onPasswordToggleClick: (Boolean) -> Unit = {},
+    onValueChange: (String) -> Unit
+) {
+    val isPasswordToggleDisplayed by remember {
+        mutableStateOf(keyboardType == KeyboardType.Password)
+    }
+
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+    ) {
+        TextField(
+            value = text,
+            onValueChange = {
+                if (it.length <= maxLength) {
+                    onValueChange(it)
+                }
+            },
+            placeholder = {
+                Text(
+                    text = hint,
+                    style = MaterialTheme.typography.body1
+                )
+            },
+            isError = error != "",
+            keyboardOptions = KeyboardOptions(
+                keyboardType = keyboardType
+            ),
+            visualTransformation = if (!showPasswordToggle && isPasswordToggleDisplayed) {
+                PasswordVisualTransformation()
+            } else {
+                VisualTransformation.None
+            },
+            singleLine = true,
+            trailingIcon = {
+                if (isPasswordToggleDisplayed) {
+                    IconButton(
+                        onClick = {
+                            onPasswordToggleClick(!showPasswordToggle)
+                        },
+                    ) {
+                        Icon(
+                            imageVector = if (showPasswordToggle) {
+                                Icons.Filled.VisibilityOff
+                            } else {
+                                Icons.Filled.Visibility
+                            },
+                            tint = Color.White,
+                            contentDescription = if (showPasswordToggle) {
+                                stringResource(id = R.string.password_visible_content_description)
+                            } else {
+                                stringResource(id = R.string.password_hidden_content_description)
+                            }
+                        )
+                    }
+                }
+            },
+        )
+        if(error.isNotEmpty()) {
+            Text(
+                text = error,
+                style = MaterialTheme.typography.body2,
+                color = MaterialTheme.colors.error,
+                textAlign = TextAlign.End,
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/socialmediaapp/presentation/login/LoginScreen.kt
@@ -0,0 +1,102 @@
+package com.example.socialmediaapp.presentation.login
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.example.socialmediaapp.presentation.components.StandardTextField
+import com.example.socialmediaapp.presentation.ui.theme.SpaceLarge
+import com.example.socialmediaapp.presentation.ui.theme.SpaceMedium
+import com.example.socialmediaapp.R
+
+@Composable
+fun LoginScreen(
+    navController: NavController,
+    viewModel: LoginViewModel = hiltViewModel()
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(
+                start = SpaceLarge,
+                end = SpaceLarge,
+                top = SpaceLarge,
+                bottom = 50.dp
+            )
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .align(Alignment.Center),
+        ) {
+            Text(
+                text = stringResource(id = R.string.login),
+                style = MaterialTheme.typography.h1
+            )
+            Spacer(modifier = Modifier.height(SpaceMedium))
+            StandardTextField(
+                text = viewModel.usernameText.value,
+                onValueChange = {
+                    viewModel.setUsernameText(it)
+                },
+                error = viewModel.usernameError.value,
+                hint = stringResource(id = R.string.login_hint)
+            )
+            Spacer(modifier = Modifier.height(SpaceMedium))
+            StandardTextField(
+                text = viewModel.passwordText.value,
+                onValueChange = {
+                    viewModel.setPasswordText(it)
+                },
+                hint = stringResource(id = R.string.password_hint),
+                keyboardType = KeyboardType.Password,
+                error = viewModel.passwordError.value,
+                showPasswordToggle = viewModel.showPassword.value,
+                onPasswordToggleClick = {
+                    viewModel.setShowPassword(it)
+                }
+            )
+            Spacer(modifier = Modifier.height(SpaceMedium))
+            Button(
+                onClick = { },
+                modifier = Modifier
+                    .align(Alignment.End)
+            ) {
+                Text(
+                    text = stringResource(id = R.string.login),
+                    color = MaterialTheme.colors.onPrimary
+                )
+            }
+        }
+        Text(
+            text = buildAnnotatedString {
+                append(stringResource(id = R.string.dont_have_an_account_yet))
+                append(" ")
+                val signUpText = stringResource(id = R.string.sign_up)
+                withStyle(
+                    style = SpanStyle(
+                        color = MaterialTheme.colors.primary
+                    )
+                ) {
+                    append(signUpText)
+                }
+            },
+            style = MaterialTheme.typography.body1,
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+        )
+    }
+
+}
