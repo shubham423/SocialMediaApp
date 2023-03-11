bbafbaa9bd62640f6de87d23a67b7c1e222802d9
shubham423 <shubh991172@gmail.com>
2023-03-11 20:02:26 +0530

    register screen

---
+++ app/src/main/java/com/example/socialmediaapp/presentation/register/RegisterScreen.kt
@@ -0,0 +1,84 @@
+package com.example.socialmediaapp.presentation.register
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.example.socialmediaapp.R
+import com.example.socialmediaapp.presentation.components.StandardTextField
+import com.example.socialmediaapp.presentation.ui.theme.SpaceLarge
+import com.example.socialmediaapp.presentation.ui.theme.SpaceMedium
+
+@Composable
+fun RegisterScreen(
+    navController: NavController,
+    viewModel: RegisterViewModel = hiltViewModel()
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(
+                start = SpaceLarge,
+                end = SpaceLarge,
+                top = SpaceLarge,
+                bottom = 50.dp
+            )
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .align(Alignment.Center),
+        ) {
+            Text(
+                text = stringResource(id = R.string.login),
+                style = MaterialTheme.typography.h1
+            )
+            Spacer(modifier = Modifier.height(SpaceMedium))
+            StandardTextField(
+                text = viewModel.usernameText.value,
+                onValueChange = {
+                    viewModel.setUsernameText(it)
+                },
+                hint = stringResource(id = R.string.login_hint)
+            )
+            Spacer(modifier = Modifier.height(SpaceMedium))
+            StandardTextField(
+                text = viewModel.passwordText.value,
+                onValueChange = {
+                    viewModel.setPasswordText(it)
+                },
+                hint = stringResource(id = R.string.password_hint),
+                keyboardType = KeyboardType.Password
+            )
+        }
+        Text(
+            text = buildAnnotatedString {
+                append(stringResource(id = R.string.dont_have_an_account_yet))
+                append(" ")
+                val signUpText = stringResource(id = R.string.sign_up)
+                withStyle(
+                    style = SpanStyle(
+                        color = MaterialTheme.colors.primary
+                    )
+                ) {
+                    append(signUpText)
+                }
+            },
+            style = MaterialTheme.typography.body1,
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+        )
+    }
+
+}
